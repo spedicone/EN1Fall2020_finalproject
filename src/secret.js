fd28e6cca23c218661749312ee27ee75de0d5870
Stefan Pedicone <stefan.pedicone@tufts.edu>
2020-12-01 12:42:41 -0500

    Added keys

--- src/secret.js
+++ src/secret.js
@@ -16,8 +16,8 @@ var getAPIusername = ""; // fill in with your name (if left blank, assuming usin
 // (2) Hardcode your API key information here
 
 // AIRTABLE SUPPORT
-var airtable_api = "key" + "XXXXXXXXXXX";     // fill in your info
-var airtable_baseid = "app" + "YYYYYYYYYYYY"; // fill in your info
+var airtable_api = "key" + "E5Ptl4VeE1e3dp";     // fill in your info
+var airtable_baseid = "app" + "Yz7kgfiofyBHvN"; // fill in your info
 
 // SYSTEMLINK SUPPORT
 var systemlink_api = "XXXXXXXXXXXXXXXXXX"; // fill in your info
